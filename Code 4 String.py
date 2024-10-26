99c99fb6b461d4c669e8a28e0efc3277d75acc75
BenBrooke450 <bensir@mail.com>
2024-10-26 23:28:18 +0200

    Code 4

---
+++ Code 4 String.py
@@ -0,0 +1,24 @@
+b = "banana"
+
+for i in b:
+    print(i)
+
+
+x = 0
+
+for i in b:
+    x = x + 1
+    print(x)
+
+
+for ben in 'brooke':
+    print(ben)
+
+
+print(b[1:])
+
+
+a = b.find("a")
+print(a)
+
+
