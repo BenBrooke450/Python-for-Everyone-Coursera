171521d532695c355339062ed0c4fe7ad5908f4b
BenBrooke450 <bensir@mail.com>
2024-10-26 17:05:58 +0200

    Code 2

---
+++ Code 2 Foundation.py
@@ -0,0 +1,27 @@
+for i in range(5):
+    print(i)
+    if i < 5:
+        print("Done")
+
+
+for i in range(10):
+    if i < 8:
+        print(i)
+    print("ONE MORE TOGO")
+
+
+for i in range(10):
+    if i < 8:
+        print(i,"Below 8")
+    if i > 8:
+        print(i, "Above 8")
+
+x = 5
+x = 6
+x = 4
+if x > 5:
+    print ("BIGGER THAN 5")
+elif x < 5:
+    print ("LOWER THAN 5")
+else:
+    print("MUST BE 5")
